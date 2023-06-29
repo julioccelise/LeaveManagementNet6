406da0a07e9b7b5e22b3a429285202dbae3c7613
ITX\jcelis <jcelis@itx.com>
2023-06-29 14:56:44 +0200

    Using view models on LeaveType

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Default Number Of Days")]
+        [Range(1, 25, ErrorMessage ="Enter a valid number")]
+        public int DefaultDays { get; set; }
+    }
+}
