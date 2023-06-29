259cbfba79dd0cdec0f8999d95d72809a549fd53
ITX\jcelis <jcelis@itx.com>
2023-06-29 19:01:01 +0200

    Using repositories with interfaces and generics

---
+++ LeaveManagement.Web/Repositories/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LeaveManagement.Web.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext context;
+
+        public GenericRepository(ApplicationDbContext applicationDbContext)
+        {
+            this.context = applicationDbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var row = await GetAsync(id);
+            if (row != null)
+            {
+                context.Set<T>().Remove(row);
+            }
+
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var row = await GetAsync(id);
+
+            return row != null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetAsync(int? id)
+        {
+            if(id == null)
+            {
+                return null;
+            }
+
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+    }
+}
